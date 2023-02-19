72f9812622a7b119773df5cc7e51744c27adca3f
Abuzar-Mehdi <abuzarmehdi.91@gmail.com>
2023-02-19 20:05:51 +0500

    first commit

--- src/main/java/com/example/devopsautomation/DevopsAutomationApplication.java
+++ src/main/java/com/example/devopsautomation/DevopsAutomationApplication.java
@@ -2,10 +2,17 @@ package com.example.devopsautomation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DevopsAutomationApplication {
 
+	@GetMapping("/test")
+	public String test(){
+		return "devops automation";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsAutomationApplication.class, args);
 	}
